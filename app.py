6ec613e68092c989b34853134d124d1b2bf7f044
gralfp <gralfp@web.de>
2021-12-28 20:36:30 +0100

    cleaned up

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import csv
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 import requests
 from pager import Pager
 
+MEDIA_FOLDER='//nas/Fotos/FotoScans/Scanned_Pictures/Hofbilder/'
 
 def read_table(url):
     """Return a list of dict"""
@@ -13,11 +14,12 @@ def read_table(url):
 
 APPNAME = "Photoalbum"
 STATIC_FOLDER = 'example'
-TABLE_FILE = "example/fakecatalog.csv"
+TABLE_FILE = "example/fakecatalog2.csv"
 
 table = read_table(TABLE_FILE)
 pager = Pager(len(table))
 
+print(table[1])
 
 app = Flask(__name__, static_folder=STATIC_FOLDER)
 app.config.update(
@@ -36,12 +38,20 @@ def image_view(ind=None):
         return render_template("404.html"), 404
     else:
         pager.current = ind
+        xx=table[ind]
+        print(xx['name'])
+        print(url_for('download_file', filename=xx['name']))
         return render_template(
             'imageview.html',
             index=ind,
             pager=pager,
             data=table[ind])
 
+
+@app.route('/uploads/<path:filename>')
+def download_file(filename):
+    print(filename)
+    return send_from_directory(MEDIA_FOLDER, filename, as_attachment=True)
 
 @app.route('/goto', methods=['POST', 'GET'])    
 def goto():
